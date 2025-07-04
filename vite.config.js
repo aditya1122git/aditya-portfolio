a57306eea0137d2477e90055351e349644b39a36
Aditya Raj <ad857885@gmail.com>
2025-07-04 18:44:52 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,17 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+          emailjs: ['@emailjs/browser']
+        }
+      }
+    }
+  },
+  optimizeDeps: {
+    include: ['@emailjs/browser']
+  }
 })
